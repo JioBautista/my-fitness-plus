58fbf1627e74cda5c3e2337f0a7617eabf01b964
Jio Bautista <98562544+JioBautista@users.noreply.github.com>
2024-01-04 08:53:35 -0500

    Updates

--- app/src/sidebar/SideBar.jsx
+++ app/src/sidebar/SideBar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "../styles/sidebar.module.scss";
-import SideBarContents from "./SideBarContents";
+import "animate.css"
 
 function SideBar() {
   const [toggle, setToggle] = React.useState(false);
@@ -15,8 +15,35 @@ function SideBar() {
 
       {toggle && (
         <>
-          <div className={styles.overlay} onClick={() => setToggle(!toggle)}></div>
-          <SideBarContents />
+        {/* OVERLAY EFFECT */}
+          <div
+            className={styles.overlay}
+            onClick={() => setToggle(!toggle)}
+          ></div>
+          {/* MOBILE SIDEBAR */}
+          <div className={`animate__animated  animate__slideInLeft ${styles.collapsed}`}>
+            <button>
+              <img src="images/icons8-workout-50.png" />
+              WORKOUTS
+            </button>
+            <button>
+              <img src="images/icons8-nutrition-32.png" />
+              NUTRITION
+            </button>
+            <button>
+              <img src="images/icons8-calculator-64.png" />
+              BMI CALCULATOR
+            </button>
+            <hr></hr>
+            <button>
+              <img src="images/icons8-user-30.png" />
+              Jbautista
+            </button>
+            <button>
+              <img src="images/icons8-settings-50.png" />
+              Settings
+            </button>
+          </div>
         </>
       )}
     </div>
