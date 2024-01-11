1f14a8b2b757c71c70cdb1b4ce1920bb4aee5fab
Jio Bautista <98562544+JioBautista@users.noreply.github.com>
2024-01-11 16:17:04 -0500

    Updates

--- app/src/pages/Workouts.jsx
+++ app/src/pages/Workouts.jsx
@@ -2,24 +2,37 @@ import React, { useEffect } from "react";
 import styles from "../styles/workouts.module.scss";
 
 function Workouts() {
-  const [workouts, setWorkouts] = React.useState([]);
-
-//   useEffect(() => {
-//     fetch("https://api.api-ninjas.com/v1/exercises?difficulty=beginner", {
-//       headers: { "X-Api-Key": "NiLaSdJJjrHz3MATgl3Tmg==DZxPhLrznogMX1ot" },
-//     })
-//       .then((res) => res.json())
-//       .then((res) => setWorkouts(res));
-//   }, []);
-
-//   console.log(workouts)
   return (
     <div className={styles.container}>
-      <h1>Workout Categories</h1>
+        <h1>Your workouts</h1>
+        <ul>
+            <li>
+                <h3>Bench Press</h3>
+
+                <div className={styles.settings}>
+                <p>Sets</p>
+                <button>-</button>
+                <span>0</span>
+                <button>+</button>
+                </div>
+
+                <div className={styles.settings}>
+                <p>Reps</p>
+                <button>-</button>
+                <span>0</span>
+                <button>+</button>
+                </div>
+            </li>
+        </ul>
+      <h2>Workout Categories</h2>
       <div className={styles.buttons}>
         <button>Beginner</button>
         <button>Intermediate</button>
         <button>Advance</button>
+      </div>
+
+      <div>
+
       </div>
     </div>
   );
