f01b2c7feb76e207b6dd9ce64e8b028421d1cadc
Jio Bautista <98562544+JioBautista@users.noreply.github.com>
2024-01-17 11:38:48 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -3,9 +3,6 @@ const app = express();
 const cors = require('cors')
 const { chestExercises, shoulderExercises } = require("./data/exerciseData");
 
-app.use(cors({
-    origin:"http://localhost:5173/"
-}))
 app.get("/chest", (req, res) => {
   res.json(chestExercises);
 });
