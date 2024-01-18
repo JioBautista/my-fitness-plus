02f8a10fc1f62b0a095d982389899409ec33d4f9
Jio Bautista <98562544+JioBautista@users.noreply.github.com>
2024-01-17 21:12:42 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const axios = require("axios");
 const cors = require("cors");
 const { chestExercises, shoulderExercises } = require("./data/exerciseData");
 
@@ -10,7 +11,38 @@ app.get("/chest", (req, res) => {
 });
 
 app.get("/shoulders", (req, res) => {
-  res.json(shoulderExercises);
+  // res.json(shoulderExercises);
+  // const options = {
+  //   method: "GET",
+  //   url: "https://api.api-ninjas.com/v1/exercises?muscle=shoulders",
+  //   headers: {
+  //     "X-Api-Key": "NiLaSdJJjrHz3MATgl3Tmg==DZxPhLrznogMX1ot",
+  //   },
+  // };
+  // axios.request(options).then((response) => res.json(response.data));
+
+  axios({
+    method: "get",
+    url: "https://api.api-ninjas.com/v1/exercises?muscle=shoulders",
+    headers: {
+      "X-Api-Key": "NiLaSdJJjrHz3MATgl3Tmg==DZxPhLrznogMX1ot",
+    },
+  })
+    .then((response) => res.json(response.data))
+    .catch((err) => console.log(err));
+
+    
+});
+
+app.get("/test", (req, res) => {
+  const options = {
+    method: "GET",
+    url: "https://api.api-ninjas.com/v1/exercises?muscle=biceps",
+    headers: {
+      "X-Api-Key": "NiLaSdJJjrHz3MATgl3Tmg==DZxPhLrznogMX1ot",
+    },
+  };
+  axios.request(options).then((response) => res.json(response.data));
 });
 
 app.listen(3000);
