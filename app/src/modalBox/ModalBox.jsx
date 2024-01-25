2031c8f9509977ba2e857f46c0ee518d2208beb8
Jio Bautista <98562544+JioBautista@users.noreply.github.com>
2024-01-25 18:39:17 -0500

    Updates

--- app/src/modalBox/ModalBox.jsx
+++ app/src/modalBox/ModalBox.jsx
@@ -2,41 +2,24 @@ import React from "react";
 import styles from "../styles/modalbox.module.scss";
 
 function ModalBox({ closeModal, serverData }) {
-  const days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
   return (
     <div className={styles.container}>
       <div className={styles.wrapper}>
         <h1>Program Info</h1>
         <table>
           <tbody>
-            {serverData[0].exercises.map((items) => (
-              <>
-                <tr>
-                  <th>{items}</th>
-                  <td>3 sets of 8 to 12 reps</td>
-                </tr>
-              </>
-            ))}
-          </tbody>
-        </table>
-
-        <table>
-          <tbody>
-            {serverData[0].sessions.map((items) => (
-              <>
-                <tr>
-                  <td>{items}</td>
-                </tr>
-              </>
-            ))}
+            {serverData ? (
+              serverData.exercises.map((items) => (
+                <React.Fragment>
+                  <tr>
+                    <th>{items}</th>
+                    <th>3 sets of 8 to 12 reps</th>
+                  </tr>
+                </React.Fragment>
+              ))
+            ) : (
+              <th>Error</th>
+            )}
           </tbody>
         </table>
         <button>Start Program</button>
--- server/data/programsData.js
+++ server/data/programsData.js
@@ -1,23 +1,20 @@
-const fullBodyWorkout = [
-  {
-    exercises: [
-      "Barbell Bench press",
-      "Barbell Military Press",
-      "Barbell Rows",
-      "Barbell Squat",
-      "Bicep Curls",
-      "Tricep Extensions",
-    ],
-    sessions: [
-      "Rest",
-      "Fullbody",
-      "Rest",
-      "Fullbody",
-      "Rest",
-      "Fullbody",
-      "Rest",
-    ],
-  },
-];
-
+const fullBodyWorkout = {
+  exercises: [
+    "Barbell Bench press",
+    "Barbell Military Press",
+    "Barbell Rows",
+    "Barbell Squat",
+    "Bicep Curls",
+    "Tricep Extensions",
+  ],
+  sessions: [
+    "Rest",
+    "Fullbody",
+    "Rest",
+    "Fullbody",
+    "Rest",
+    "Fullbody",
+    "Rest",
+  ],
+};
 module.exports = { fullBodyWorkout };
