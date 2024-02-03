4fecc2e9159d95e8b3cefb3c275a8921b83e087a
Jio Bautista <98562544+JioBautista@users.noreply.github.com>
2024-02-03 18:08:14 -0500

    Update SignUpForm.jsx

--- app/src/loginForm/SignUpForm.jsx
+++ app/src/loginForm/SignUpForm.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 import styles from "../styles/loginform.module.scss";
 import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
 
 function SignUpForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
   return (
     <div className={styles.wrapper}>
       <h2>Create an account</h2>
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <label>Create a username</label>
         <input />
         <label>Enter a password</label>
